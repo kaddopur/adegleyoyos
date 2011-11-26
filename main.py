d91bd32e87a4e16c2f0a9972b5e5152347486cbc
Jason C. Huang <chaoju.huang@gmail.com>
2011-11-26 16:27:46 +0800

    - index.html - contact.html

---
+++ main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#
+# Copyright 2011 Adegle Inc.
+#
+
+import cgi
+
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
+import os
+from google.appengine.ext.webapp import template
+
+class MainPage(webapp.RequestHandler):
+  def get(self):
+    template_values = {}
+    path = os.path.join(os.path.dirname(__file__), 'index.html')
+    self.response.out.write(template.render(path, template_values))
+
+    
+class ContactPage(webapp.RequestHandler):
+  def get(self):
+    template_values = {}
+    path = os.path.join(os.path.dirname(__file__), 'contact.html')
+    self.response.out.write(template.render(path, template_values))
+    
+
+application = webapp.WSGIApplication([('/', MainPage),
+                                      ('/contact', ContactPage)],
+                                     debug=True)
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
