0affca117dce72f15e2b9ddfaec78725148d8fbd
KeDouBo <kaddo.dev@gmail.com>
2012-11-21 18:19:02 +0800

    Ready to release v3.1

--- src/wake.rb
+++ src/wake.rb
@@ -1,4 +1,4 @@
 watch('.*\.coffee') {|md| system("coffee -bc -o ../js #{md[0]}")}
-watch('.*\.jade')   {|md| system("jade -P -O .. #{md[0]}")}
+watch('.*\.jade')   {|md| system("jade -P -O .. .")}
 watch('.*\.styl')   {|md| system("stylus -o ../css #{md[0]}")}
 
